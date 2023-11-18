a490d8438aa1b6ad321eead412dbe84dcfb275be
Feisal2023 <feisaladan2022@gmail.com>
2023-11-18 14:57:43 +0300

    completed react template

---
+++ src/redux/store.js
@@ -0,0 +1,7 @@
+import { configureStore } from "@reduxjs/toolkit";
+import dataReducer from "../redux/features/data/dataSlice";
+export const store = configureStore({
+  reducer: {
+    data: dataReducer,
+  },
+});
